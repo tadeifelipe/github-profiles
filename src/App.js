752f2298f44c0715976a0cf10741c94575977404
tadeifelipe <felipe_santostadei@hotmail.com>
2020-05-25 21:00:13 -0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Global.css';
+
+import Header from './components/Header/Header';
 
 function App() {
   return (
-   <h1>Hello World</h1> 
+   <Header/>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+const Header = () =>{
+    return(
+        <div className="container">
+            <div>Github Profiles</div>
+            <div>
+                <button className="button"></button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
