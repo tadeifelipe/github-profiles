71b71205ede99e6fdd0b1a6c580e293ff576ae17
tadeifelipe <felipe_santostadei@hotmail.com>
2020-05-27 23:29:27 -0300

    add search api

--- src/components/Shearch/Search.js
+++ src/components/Shearch/Search.js
@@ -6,6 +6,7 @@ class Search extends Component {
         super(props);
 
         this.state = {
+            username: '',
             name: '',
             avatar_url: '',
             followers: '',
@@ -15,14 +16,50 @@ class Search extends Component {
             ]
         }
     }
+
+    handleInput = (event) => {
+        const { name, value} = event.target;
+        this.setState({
+            [name]:[value]
+        })
+    }
+
+    handleSearch = () =>{
+       fetch(`https://api.github.com/users/${this.state.username}`)
+       .then(res => {
+           return res.json();
+       })
+       .then(res => {
+            console.log(res);
+
+            //find repos
+            fetch(`https://api.github.com/users/${this.state.username}/repos`)
+            .then(res => {
+                return res.json();        
+            })
+            .then(res => {
+                for(const repo of res){
+                    console.log(repo);
+                }
+            })
+            .catch(err => console.log(err));
+       })
+       .catch(err => console.log(err));
+    }
     
     render(){
+        const { username } = this.state;
         return (
             <>
             <div className="find">
                 <form className="form">
-                    <input type="text" placeholder="username" className="username"></input>
-                    <button type="button" className="search"></button>
+                    <input type="text" 
+                           placeholder="username" 
+                           className="username" 
+                           name ="username" 
+                           value={username}
+                           onChange={this.handleInput}></input>
+                    <button type="button" className="search" onClick={this.handleSearch}></button>
                 </form>               
             </div>
             <div className="profile">
